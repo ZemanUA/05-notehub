6a684731836fd03da1bcb2ed46ef646b8d9ee137
Valentyn Ziomenko <ziomenkow@gmail.com>
2026-07-15 23:07:51 +0200

    first commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import css from'./App.module.css'
+
+export default function App() {
+
+  return (
+    <>
+  <div className={css.app}>
+	<header className={css.toolbar}>
+		{/* Компонент SearchBox */}
+		{/* Пагінація */}
+		{/* Кнопка створення нотатки */}
+  </header>
+</div>
+
+    </>
+  )
+}
+
+
